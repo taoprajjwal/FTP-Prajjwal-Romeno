920a4ac8d683ec19f50535afb876a3dc1e3b7596
wenogk <drf325@nyu.edu>
2021-03-05 19:07:35 +0400

    Getting user input as separate strings

--- FTPClient.c
+++ FTPClient.c
@@ -15,13 +15,14 @@
 int main(int argc, char *argv[])
 {
     printf("%s %s \n", argv[1], argv[2]);
+
     while (1)
     {
-        char currentInputString[MAX_STRING_SIZE];
-        //constant loop for user input
-        printf("ftp >");
-        scanf("%s", currentInputString);
-        printf("%s\n", currentInputString);
+        char currentUserInputArray[2][MAX_STRING_SIZE];
+        //constant loop for getting the user's input commands
+        printf("ftp > ");
+        scanf("%s %[^\n]s", currentUserInputArray[0], currentUserInputArray[1]);
+        printf("%s --- %s \n", currentUserInputArray[0], currentUserInputArray[1]);
     }
     return 0;
 }
