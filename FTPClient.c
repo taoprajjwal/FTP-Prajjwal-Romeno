016ffe14e0bbfd711a173539c1ab917bb7d6acc8
wenogk <drf325@nyu.edu>
2021-03-05 20:14:40 +0400

    Parsing user input for commands for username and password input and error message is command is not recognized

--- FTPClient.c
+++ FTPClient.c
@@ -10,7 +10,8 @@
 #include <sys/select.h>
 #include <sys/time.h>
 
-#define MAX_STRING_SIZE 30
+#define MAX_STRING_WORD_SIZE 30
+#define MAX_STRING_SIZE 60
 
 int main(int argc, char *argv[])
 {
@@ -18,11 +19,23 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-        char currentUserInputArray[2][MAX_STRING_SIZE];
+        char currentUserInputArray[2][MAX_STRING_WORD_SIZE];
         //constant loop for getting the user's input commands
         printf("ftp > ");
         scanf("%s %[^\n]s", currentUserInputArray[0], currentUserInputArray[1]);
-        printf("%s --- %s \n", currentUserInputArray[0], currentUserInputArray[1]);
+
+        if (strcmp(currentUserInputArray[0], "USER") == 0 && currentUserInputArray[1][0] != '\0')
+        {
+            printf("Username command ---> %s --- %s \n", currentUserInputArray[0], currentUserInputArray[1]);
+        }
+        else if (strcmp(currentUserInputArray[0], "PASS") == 0 && currentUserInputArray[1][0] != '\0')
+        {
+            printf("Password command ---> %s --- %s \n", currentUserInputArray[0], currentUserInputArray[1]);
+        }
+        else
+        {
+            printf("Command not recognized. \n");
+        }
     }
     return 0;
 }
